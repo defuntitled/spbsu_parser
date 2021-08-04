7b60dc6e397575e6f9111c11f40c4986edd6f450
ded vilir <kirya.bikeev.03@mail.ru>
2021-08-04 14:04:14 +0700

    simple refactor

--- parser.py
+++ parser.py
@@ -17,31 +17,21 @@ def pars(root):
         td_list = tr.select('td')
         if not td_list:
             continue
-        snils, score = td_list[1], td_list[4]
-        abit.append((snils.text, score.text))
+        snils, score, accept = td_list[1].text, td_list[4].text, td_list[10].text
+        abit.append((snils, score, accept))
     return abit
 
 
-def accept_rate(root, my_score):
-    table = root.find('tbody')
-    abit = []
-    for tr in table.select('tr'):
-        td_list = tr.select('td')
-        if not td_list:
-            continue
-        snils, score = td_list[1], td_list[4]
-        if (score.text != ''):
-            score = float(score.text.replace(',', '.'))
-        else:
-            score = 4000
-        if td_list[10].text == "Да" and score >= my_score:
-            abit.append((snils.text, score))
+def accept_rate(full_abit, my_score):
+    abit = list(
+        filter(lambda x: (x[2] == "Да" and x[1] == '') or (x[2] == "Да" and float(x[1].replace(',', '.')) >= my_score),
+               full_abit))
     return abit
 
 
 table_url = input("url of the competition table: ")
 my_score = int(input("your score: "))
-result = accept_rate(mk_soup(table_url),
+result = accept_rate(pars(mk_soup(table_url)),
                      my_score)
 print(len(result))
 print(*result)
